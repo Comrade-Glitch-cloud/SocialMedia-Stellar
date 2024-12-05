ae3a6ba1f488c2831d17647d1839f32263ca2612
EC2 Default User <ec2-user@ip-172-31-21-144.eu-west-1.compute.internal>
2024-12-05 20:25:18 +0000

    static3

--- stellar/settings.py
+++ stellar/settings.py
@@ -93,9 +93,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    # Add other directories if needed
 ]
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'

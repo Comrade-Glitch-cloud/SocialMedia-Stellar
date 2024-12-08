df31b360f7ec1f5f05c6277de90f60361ea1e1da
EC2 Default User <ec2-user@ip-172-31-21-144.eu-west-1.compute.internal>
2024-12-08 00:43:30 +0000

    changes

--- star_accounts_hub/views.py
+++ star_accounts_hub/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.views.decorators.http import require_http_methods
 from .models import CadetProfile
 from django.contrib.auth.models import User
 
@@ -12,37 +13,33 @@ class DockUp(CreateView):
     template_name = 'star_accounts_hub/dockup.html'
 
 @login_required
+@require_http_methods(["GET", "POST"])
 def cadetprofile(request):
-    if not hasattr(request.user, 'cadet'):
-        if not CadetProfile.objects.filter(user=request.user).exists():
-            CadetProfile.objects.create(user=request.user, stellarname=request.user.username)
+    # Ensure cadet profile exists for the user
+    cadet_profile, created = CadetProfile.objects.get_or_create(user=request.user, defaults={'stellarname': request.user.username})
 
     if request.method == 'POST':
-        stellarname = request.POST.get('stellarname')
-        email = request.POST.get('email')
-        first_name = request.POST.get('first_name')
-        last_name = request.POST.get('last_name')
+        cadet_profile.stellarname = request.POST.get('stellarname')
+        request.user.email = request.POST.get('email')
+        request.user.first_name = request.POST.get('first_name')
+        request.user.last_name = request.POST.get('last_name')
         cadet_picture = request.FILES.get('cadet_picture')
 
-        request.user.cadet.stellarname = stellarname
-        request.user.email = email
-        request.user.first_name = first_name
-        request.user.last_name = last_name
         if cadet_picture:
-            request.user.cadet.cadet_picture = cadet_picture
-        request.user.cadet.save()
+            cadet_profile.cadet_picture = cadet_picture
+
+        cadet_profile.save()
         request.user.save()
 
         return redirect('star_accounts_hub:cadetdetails')
 
-    cadet_profile = CadetProfile.objects.get(user=request.user)
     context = {'cadet': cadet_profile}
     return render(request, 'star_accounts_hub/cadetprofile.html', context)
 
 @login_required
+@require_http_methods(["GET"])
 def cadetdetails(request):
-    if not hasattr(request.user, 'cadet'):
-        if not CadetProfile.objects.filter(user=request.user).exists():
-            CadetProfile.objects.create(user=request.user, stellarname=request.user.username)
-    user_cadet = request.user.cadet
-    return render(request, 'star_accounts_hub/cadetdetails.html', {'cadet': user_cadet})
+    # Ensure cadet profile exists for the user
+    cadet_profile, created = CadetProfile.objects.get_or_create(user=request.user, defaults={'stellarname': request.user.username})
+
+    return render(request, 'star_accounts_hub/cadetdetails.html', {'cadet': cadet_profile})
